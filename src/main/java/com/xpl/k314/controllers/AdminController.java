cd50f84a752deeeeb3a0ffdaae6ea48a0d6a01ef
masamune <emopanda1337@gmail.com>
2023-10-26 11:40:25 +0700

    + delete & put methods

--- src/main/java/com/xpl/k314/controllers/AdminController.java
+++ src/main/java/com/xpl/k314/controllers/AdminController.java
@@ -7,10 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -34,7 +36,7 @@ public class AdminController {
         return "admin";
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public String userEdit(@ModelAttribute("user") User user,
                            @RequestParam("roleIds") List<Integer> roleIds) {
         userService.saveUserWithRoles(user, roleIds);
@@ -48,7 +50,7 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-    @GetMapping("/{id}")
+    @DeleteMapping("/{id}")
     public String userDelete(@PathVariable("id") int id) {
         userService.deleteUser(id);
         return "redirect:/admin";
